a75c080a189f0b56929fc08fe5d939b8a626c97c
changawa-antony <antony.changawa@student.moringaschool.com>
2021-05-31 03:09:16 +0300

    Created menu for users

---
+++ interaction.py
@@ -0,0 +1,19 @@
+
+
+class Menu:
+     
+ '''
+ Class for the Menu items
+ '''
+ def __init__(self):
+      self.accountdata = allacounts() #imported class from app module
+ 
+      self.choices = {    #menu items that will be accessed 
+ 
+           "1" : self.show_accounts,
+           "2" : self.add_account,
+           "3" : self.search_account,
+           "4" : self.delete_account,
+           "5" : self.password_generator,
+           "6" : self.quit
+        }
